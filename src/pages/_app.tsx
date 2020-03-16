ce8e26ad68b497ed181c584e599723308235d6c6
Jonathan Gongye <mail@yefoenix.ws>
2020-03-16 18:57:35 +0800

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,7 @@ export default ({ Component, pageProps }) => (
     <Component {...pageProps} />
     <footer>
       <abbr title="This site and all its content are licensed under a Creative Commons Attribution-NonCommercial 4.0 International License.">CC BY-NC 4.0</abbr>
-      {` ${(new Date).getFullYear()} © Shu Ding.`}
+      {` ${(new Date).getFullYear()} Via. Shu Ding.`}
     </footer>
   </>
 )
